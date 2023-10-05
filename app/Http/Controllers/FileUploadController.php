3bcd320f4a0e8bbf65e42ccf39a5b9f50a3a831f
yash <yashsrivastava0407@gmail.com>
2023-10-05 12:36:55 +0530

    Upload Project

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -79,22 +79,31 @@ class FileUploadController extends Controller
         $content = strip_tags($content);
         }
         else{
-            $fileHandle = fopen($request->file->path(), "r");
-            $line = @fread($fileHandle, filesize($request->file->path()));   
-            $lines = explode(chr(0x0D),$line);
-            $outtext = "";
-            foreach($lines as $thisline)
-              {
-                $pos = strpos($thisline, chr(0x00));
-                if (($pos !== FALSE)||(strlen($thisline)==0))
-                  {
-                  } else {
-                    $outtext .= $thisline." ";
-                  }
-              }
-             $content = preg_replace("/[^a-zA-Z0-9\s\,\.\-\n\r\t@\/\_\(\)]/","",$outtext);
+            if(($fh = fopen($request->file->path(), 'r')) !== false ) 
+        {
+           $headers = fread($fh, 0xA00);
+
+           // 1 = (ord(n)*1) ; Document has from 0 to 255 characters
+           $n1 = ( ord($headers[0x21C]) - 1 );
+
+           // 1 = ((ord(n)-8)*256) ; Document has from 256 to 63743 characters
+           $n2 = ( ( ord($headers[0x21D]) - 8 ) * 256 );
+
+           // 1 = ((ord(n)*256)*256) ; Document has from 63744 to 16775423 characters
+           $n3 = ( ( ord($headers[0x21E]) * 256 ) * 256 );
+
+           // 1 = (((ord(n)*256)*256)*256) ; Document has from 16775424 to 4294965504 characters
+           $n4 = ( ( ( ord($headers[0x21F]) * 256 ) * 256 ) * 256 );
+
+           // Total length of text in the document
+           $textLength = ($n1 + $n2 + $n3 + $n4);
+
+           $extracted_plaintext = fread($fh, $textLength);
+
+           $content = $extracted_plaintext;
         }
-        dd($content);
+             
+        }
         
 
 
